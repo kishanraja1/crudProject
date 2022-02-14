30f1884fb27ddaae3e9dfddbb939e46e88181e69
Kishan Raja <kishan.raja96@gmail.com>
2022-02-14 14:44:21 -0500

    added time to add class

--- models/schedule.js
+++ models/schedule.js
@@ -6,7 +6,8 @@ const scheduleSchema = new mongoose.Schema({
   className: String,
   date: String,
   instructor: String,
-  capacity: Number
+  capacity: Number,
+  time: String
 })
 
 const scheduleCollection = mongoose.model('Class', scheduleSchema)
